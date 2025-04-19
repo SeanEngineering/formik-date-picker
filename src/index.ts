1905f0d76b2f971c39df3679424e4813125a7464
Sean <sean@MacBookPro.modem>
2025-04-20 07:59:31 +1000

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export const testScript = () => true;
+import FormikDatePicker from "./FormikDatePicker";
+import * as FormikDatePickerProps from "./props";
 
+export * from "./props"
+export * from "./FormikDatePicker";
+export default FormikDatePicker;
+
+export { FormikDatePickerProps };
